e2fda8918543f7bfd41ba86b62ee502bf243ed1a
bonjo1 <70815595+bonjo1@users.noreply.github.com>
2020-10-03 05:22:40 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Webexample</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+</head>
+<body>
+    
+    <header>
+        <div class="anim1">Welcome</div>
+        <div class="select"> 
+            <select name="slct" id="select language"> 
+                <option>Select Langugage</option> 
+                <option value="1">English</option> 
+                <option value="2">中文</option> 
+                <option value="3">Español</option> 
+                <option value="4">French</option> 
+            </select> 
+        </div> 
+        
+        </div>
+        <ul id="nav2">
+            <li><a href= "#">LOG IN</a></li>
+            <li><a href= "#">REGISTER</a></li>
+        </ul>
+        <ul id="nav1">
+            <li><a href= "#">HOW TO PURCAHSE?</a></li>
+            <li><a href= "#">GUARANTEE</a></li>
+            <li><a href= "#">REVIEWS</a></li>
+            <li><a href= "#">JOBS</a></li>
+            <li><a href= "#">CONTACT US</a></li>
+        </ul>
+    </div>
+    </header>
+    <div id="container1">
+        <div id="logo">
+            <div id="lineLong" class="line"></div>
+            <div id="lineShort"class="line"></div>
+            <div id="circleOne" class="circle"></div>
+            <div id="circleTwo" class="circle"></div>
+            <div id="circleThree" class="circle"></div>    
+        </div>
+        <div class="anim2"><h1>DOTA 2 MMR BOOSTING</h1></div>
+        <div class="text">
+        <h1> DOTA 2 BOOST</h1>
+        <h6>Premium MMR Boosting Service</h6>
+        <div id="line1" class="line"></div>
+    </div>
+    <div class="button1">
+        <class>Ranked Roles</class>
+    </div>
+    <div class="button2">
+        <class>Ranked Classic</class>
+        </div>
+        <div class="button3">
+        <class>DUO BOOSTING</class>
+        </div>
+        <div class="button4">
+        <class>PLACEMENT MATCHES</class>
+        </div>
+        <div class="button5">
+        <class>Per Win Boost</class>
+        </div>
+        <Footer>
+            <div class="leftfooter">
+            <span class="footer-copyright">© Copyright 2020. All Rights Reserved.</span>
+            </div>
+            <div class="centerfooter">
+            <i class="fa fa-paypal" aria-hidden="true"></i>
+            <i class="fa fa-cc-visa" aria-hidden="true"></i>
+            <i class="fa fa-cc-mastercard" aria-hidden="true"></i>
+            <i class="fa fa-credit-card-alt" aria-hidden="true"></i>
+            <i class="fa fa-google-wallet" aria-hidden="true"></i>
+            <i class="fa fa-credit-card" aria-hidden="true"></i>
+            <i class="fa fa-cc-discover" aria-hidden="true"></i>
+            <i class="fa fa-cc-diners-club" aria-hidden="true"></i>
+            
+            </div>
+            <div class="fadeout">
+                <h1>🤗Thank You🤗 </h1>
+            </div>
+        </Footer>
+</body>
+</html>
